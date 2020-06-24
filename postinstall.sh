2be132d071da2d60a945d9aaacebe0855ef48b83
Sam Johnson <sam@durosoft.com>
2020-06-24 19:49:11 -0400

    postinstall working

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+mkdir -p bin || exit 1
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   MINIFIER_BIN=linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   MINIFIER_BIN=macos
 fi
 echo "$MINIFIER_BIN detected, downloading minifier binary..."
-curl -L --url https://github.com/sam0x17/es6-minifier/releases/download/minify-dist/minify-$MINIFIER_BIN --output dist/minify || exit 1
-chmod +x dist/minify || exit 1
+curl -L --url https://github.com/sam0x17/es6-minifier/releases/download/minify-dist/minify-$MINIFIER_BIN --output bin/minify || exit 1
+chmod +x bin/minify || exit 1
