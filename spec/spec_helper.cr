963ceb1a6bc3fcf05ed9a98388c5a840e7034b04
Sam Johnson <sam@durosoft.com>
2020-06-24 13:37:15 -0400

    initial commit

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/es6-minifier"
---
+++ src/es6-minifier.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Es6::Minifier`
+module Es6::Minifier
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
